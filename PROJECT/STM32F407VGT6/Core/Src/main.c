ee77c2b71bb4f9ccdce5bc6ae51bb032910f03ec
oiwyhcode <1493156832@qq.com>
2025-07-19 22:20:19 +0800

    舵机始终初始化完成

--- PROJECT/STM32F407VGT6/Core/Src/main.c
+++ PROJECT/STM32F407VGT6/Core/Src/main.c
@@ -164,6 +164,8 @@ Angle_PID.target_val=0;
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_2);
   HAL_TIM_Base_Start_IT(&htim7);
   HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
   HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_ALL);
@@ -176,7 +178,8 @@ Angle_PID.target_val=0;
   while (1)
   {
 
-
+	  __HAL_TIM_SetCompare(&htim12,TIM_CHANNEL_1,100);
+	  __HAL_TIM_SetCompare(&htim12,TIM_CHANNEL_2,100);
 	  OLED_NewFrame();
 	  sprintf(message,"distance:%.2fcm",get_filtered_distance());
 	  OLED_PrintString(1, 16, message, &font16x16, 0);
